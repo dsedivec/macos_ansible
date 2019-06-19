53691a284f22da07d23d8966ef11f2ead022c43a
Dale Sedivec <dale@Dale.caliginous.net>
2019-06-19 00:01:21 -0500

    Initial build with MacPorts and Emacs

---
+++ roles/macports/files/install_macports.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+import json
+import logging
+import os
+import os.path
+import re
+import shutil
+import subprocess
+import sys
+import tempfile
+import urllib
+
+
+def main(argv):
+    logging.basicConfig(level=logging.INFO)
+    logging.info("Fetching macOS version")
+    output = subprocess.check_output(["sw_vers", "-productVersion"])
+    match = re.search(r"^\d+\.\d+", output)
+    macos_version = match.group(0)
+    logging.info("macOS version is %r", macos_version)
+    logging.info("Fetching MacPorts release information from GitHub")
+    release_info = json.load(
+        urllib.urlopen(
+            "https://api.github.com/repos/macports/macports-base/releases/latest"
+        )
+    )
+    version_regexp = re.compile(
+        r"^MacPorts-.*-%s-\w+.pkg$" % (re.escape(macos_version),)
+    )
+    logging.info("Scanning MacPorts releases")
+    for asset in release_info["assets"]:
+        if "name" in asset and version_regexp.search(asset["name"]):
+            break
+    else:
+        raise Exception("Can't find MacPorts release")
+    pkg_file_name = asset["name"]
+    logging.info("Found MacPorts release %r", pkg_file_name)
+    if "/" in pkg_file_name:
+        raise Exception("Illegal file name %r" % (pkg_file_name))
+    remote_pkg = urllib.urlopen(asset["browser_download_url"])
+    download_dir = tempfile.mkdtemp()
+    local_pkg_path = os.path.join(download_dir, pkg_file_name)
+    with open(local_pkg_path, "wb") as local_pkg:
+        shutil.copyfileobj(remote_pkg, local_pkg)
+    remote_pkg.close()
+    logging.info("Downloaded MacPorts pkg")
+    cmd = ["installer", "-pkg", local_pkg_path, "-target", "/"]
+    if os.getuid() != 0:
+        cmd.insert(0, "sudo")
+    logging.info("Running: %r" % (cmd,))
+    subprocess.check_call(cmd)
+    shutil.rmtree(download_dir)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
---
+++ roles/software_emacs/files/build_install_ns_port.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# Packages you will need:
+# rsvg, ImageMagick, libxml2, gnutls, Jansson
+
+set -euo pipefail
+
+extra_config=''
+
+if [[ $# -eq 1 && "$1" = "debug" ]]; then
+	# See etc/DEBUG in Emacs sources.
+	echo "building with debugging options"
+	export CFLAGS='-O0 -g3'
+	extra_config="--enable-checking=yes,glyphs
+	              --enable-check-lisp-object-type"
+elif [[ $# -ne 0 ]]; then
+	echo "usage: $(basename "$0") [debug]" >&2
+	exit 1
+fi
+
+export CC="ccache gcc"
+num_procs=$(getconf _NPROCESSORS_ONLN)
+export MAKEFLAGS=-j$num_procs
+
+if [ ! -f src/emacs.c ]; then
+	echo "Can't find src/emacs.c, wrong PWD?" 2>&1
+	exit 1
+fi
+
+git clean -Xdf
+./autogen.sh
+./configure --with-ns \
+            --with-modules --with-rsvg --with-imagemagick \
+            --with-xml2 --with-gnutls --with-json --without-x $extra_config
+# Must "make" before "make install", otherwise you don't get man and
+# info installed in the app bundle.
+make
+make install
+install -d ~/Applications
+dest=$HOME/Applications/Emacs.app
+if [ -e "$dest" ]; then
+	old=$HOME/Applications/Emacs\ Old.app
+	rm -rf "$old"
+	mv "$dest" "$old"
+fi
+mv nextstep/Emacs.app "$dest"
