7858f1690bbc6c381c18b9314e3a02be757469ac
Dale Sedivec <dale@codefu.org>
2019-10-24 16:34:17 -0500

    Calibre requires macOS Mojave or newer

--- filter_plugins/filters.py
+++ filter_plugins/filters.py
@@ -15,6 +15,22 @@ def named_list(the_lists, index_names):
         yield dict(zip(index_names, a_list))
 
 
+def filter_casks_by_os_version(cask_list, mac_os_minor_version):
+    """cask_list can be \"cask\" or [\"cask\", min_os_minor_version]."""
+    mac_os_minor_version = int(mac_os_minor_version)
+    for cask in cask_list:
+        if not isinstance(cask, list):
+            yield cask
+        else:
+            cask, min_version = cask
+            if mac_os_minor_version >= int(min_version):
+                yield cask
+
+
 class FilterModule(object):
     def filters(self):
-        return {"dict_product": dict_product, "named_list": named_list}
+        return {
+            "dict_product": dict_product,
+            "named_list": named_list,
+            "filter_casks_by_os_version": filter_casks_by_os_version,
+        }
